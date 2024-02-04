ce7f7fd32dd32c38226439877872fff74eea775a
NORMss <serega.b301@gmail.com>
2024-02-04 23:10:30 +0700

    feat: added simple calculator functionality
    Closes #1

---
+++ app/src/main/java/com/norm/mycalculator/ui/theme/Calculator.kt
@@ -0,0 +1,263 @@
+package com.norm.mycalculator.ui.theme
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.norm.mycalculator.CalculatorAction
+import com.norm.mycalculator.CalculatorOperation
+import com.norm.mycalculator.CalculatorState
+
+@Composable
+fun Calculator(
+    state: CalculatorState,
+    buttonSpacing: Dp = 8.dp,
+    modifier: Modifier = Modifier,
+    onAction: (CalculatorAction) -> Unit
+) {
+    Box(modifier = modifier)
+    {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .align(Alignment.BottomCenter),
+            verticalArrangement = Arrangement.spacedBy(buttonSpacing)
+        ) {
+            Text(
+                text = state.number1 + (state.operation?.symbol ?: "") + state.number2,
+                textAlign = TextAlign.End,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(vertical = 32.dp),
+                fontWeight = FontWeight.Light,
+                fontSize = 80.sp,
+                color = Color.White,
+                maxLines = 1
+            )
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth(),
+                horizontalArrangement = Arrangement.spacedBy(buttonSpacing)
+            ) {
+                OperationButton(
+                    symbol = "AC",
+                    modifier = Modifier
+                        .background(MyLightGreen)
+                        .aspectRatio(2f)
+                        .weight(2f),
+                    onClick = {
+                        onAction(CalculatorAction.Clear)
+                    }
+                )
+                OperationButton(
+                    symbol = "Del",
+                    modifier = Modifier
+                        .background(MyLightGreen)
+                        .aspectRatio(1f)
+                        .weight(1f),
+                    onClick = {
+                        onAction(CalculatorAction.Delete)
+                    }
+                )
+                OperationButton(
+                    symbol = "/",
+                    modifier = Modifier
+                        .background(MyGreen)
+                        .aspectRatio(1f)
+                        .weight(1f),
+                    onClick = {
+                        onAction(CalculatorAction.Operation(CalculatorOperation.Divide))
+                    }
+                )
+            }
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth(),
+                horizontalArrangement = Arrangement.spacedBy(buttonSpacing)
+            ) {
+                OperationButton(
+                    symbol = "1",
+                    modifier = Modifier
+                        .background(MyGreen)
+                        .aspectRatio(1f)
+                        .weight(1f),
+                    onClick = {
+                        onAction(CalculatorAction.Number(1))
+                    }
+                )
+                OperationButton(
+                    symbol = "2",
+                    modifier = Modifier
+                        .background(MyGreen)
+                        .aspectRatio(1f)
+                        .weight(1f),
+                    onClick = {
+                        onAction(CalculatorAction.Number(2))
+                    }
+                )
+                OperationButton(
+                    symbol = "3",
+                    modifier = Modifier
+                        .background(MyGreen)
+                        .aspectRatio(1f)
+                        .weight(1f),
+                    onClick = {
+                        onAction(CalculatorAction.Number(3))
+                    }
+                )
+                OperationButton(
+                    symbol = "+",
+                    modifier = Modifier
+                        .background(MyGreen)
+                        .aspectRatio(1f)
+                        .weight(1f),
+                    onClick = {
+                        onAction(CalculatorAction.Operation(CalculatorOperation.Add))
+                    }
+                )
+            }
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth(),
+                horizontalArrangement = Arrangement.spacedBy(buttonSpacing)
+            ) {
+                OperationButton(
+                    symbol = "4",
+                    modifier = Modifier
+                        .background(MyGreen)
+                        .aspectRatio(1f)
+                        .weight(1f),
+                    onClick = {
+                        onAction(CalculatorAction.Number(4))
+                    }
+                )
+                OperationButton(
+                    symbol = "5",
+                    modifier = Modifier
+                        .background(MyGreen)
+                        .aspectRatio(1f)
+                        .weight(1f),
+                    onClick = {
+                        onAction(CalculatorAction.Number(5))
+                    }
+                )
+                OperationButton(
+                    symbol = "6",
+                    modifier = Modifier
+                        .background(MyGreen)
+                        .aspectRatio(1f)
+                        .weight(1f),
+                    onClick = {
+                        onAction(CalculatorAction.Number(6))
+                    }
+                )
+                OperationButton(
+                    symbol = "-",
+                    modifier = Modifier
+                        .background(MyGreen)
+                        .aspectRatio(1f)
+                        .weight(1f),
+                    onClick = {
+                        onAction(CalculatorAction.Operation(CalculatorOperation.Subtract))
+                    }
+                )
+            }
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth(),
+                horizontalArrangement = Arrangement.spacedBy(buttonSpacing)
+            ) {
+                OperationButton(
+                    symbol = "7",
+                    modifier = Modifier
+                        .background(MyGreen)
+                        .aspectRatio(1f)
+                        .weight(1f),
+                    onClick = {
+                        onAction(CalculatorAction.Number(7))
+                    }
+                )
+                OperationButton(
+                    symbol = "8",
+                    modifier = Modifier
+                        .background(MyGreen)
+                        .aspectRatio(1f)
+                        .weight(1f),
+                    onClick = {
+                        onAction(CalculatorAction.Number(7))
+                    }
+                )
+                OperationButton(
+                    symbol = "9",
+                    modifier = Modifier
+                        .background(MyGreen)
+                        .aspectRatio(1f)
+                        .weight(1f),
+                    onClick = {
+                        onAction(CalculatorAction.Number(7))
+                    }
+                )
+                OperationButton(
+                    symbol = "x",
+                    modifier = Modifier
+                        .background(MyGreen)
+                        .aspectRatio(1f)
+                        .weight(1f),
+                    onClick = {
+                        onAction(CalculatorAction.Operation(CalculatorOperation.Multiply))
+                    }
+                )
+            }
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth(),
+                horizontalArrangement = Arrangement.spacedBy(buttonSpacing)
+            ) {
+                OperationButton(
+                    symbol = "0",
+                    modifier = Modifier
+                        .background(MyGreen)
+                        .aspectRatio(2f)
+                        .weight(2f),
+                    onClick = {
+                        onAction(CalculatorAction.Number(0))
+                    }
+                )
+                OperationButton(
+                    symbol = ".",
+                    modifier = Modifier
+                        .background(MyGreen)
+                        .aspectRatio(1f)
+                        .weight(1f),
+                    onClick = {
+                        onAction(CalculatorAction.Decimal)
+                    }
+                )
+                OperationButton(
+                    symbol = "=",
+                    modifier = Modifier
+                        .background(MyLightGreen)
+                        .aspectRatio(1f)
+                        .weight(1f),
+                    onClick = {
+                        onAction(CalculatorAction.Calculate)
+                    }
+                )
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/norm/mycalculator/ui/theme/Color.kt
@@ -0,0 +1,12 @@
+package com.norm.mycalculator.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+val MyRed = Color(0xFFFF9273)
+val MyGreen = Color(0xFFB3F36D)
+val MyLightGreen = Color(0xFF62DA9A)
+val MyBlue = Color(0xFF64AAD0)
